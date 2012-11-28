d1054bbba58d9f51599ccf82af0715f8e8209d49
Tse-Ching Ho <tsechingho@gmail.com>
2012-11-28 23:21:37 +0800

    bundle gem icomoon-rails

---
+++ lib/icomoon-rails.rb
@@ -0,0 +1,7 @@
+require "icomoon-rails/version"
+
+module Icomoon
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/icomoon-rails/version.rb
@@ -0,0 +1,5 @@
+module Icomoon
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
